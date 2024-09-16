fbab17b9801bd03a118c3e0b43d2cc9dea7a6d41
idiot <joewilliamdd31@gmail.com>
2024-09-16 14:35:42 +0100

    Create DropdownItem component

--- resources/js/Components/Dropdown.tsx
+++ resources/js/Components/Dropdown.tsx
@@ -122,8 +122,27 @@ const DropdownLink = ({
   );
 };
 
+const DropdownItem = ({
+  className = "",
+  children,
+  ...props
+}: React.AllHTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      {...props}
+      className={
+        "block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out " +
+        className
+      }
+    >
+      {children}
+    </div>
+  );
+};
+
 Dropdown.Trigger = Trigger;
 Dropdown.Content = Content;
 Dropdown.Link = DropdownLink;
+Dropdown.Item = DropdownItem;
 
 export default Dropdown;
