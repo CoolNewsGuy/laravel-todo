e05027dbdb96ebd043117b1369ed7b09fbb28a08
idiot <joewilliamdd31@gmail.com>
2024-09-16 02:45:27 +0100

    Create Todo interface

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -7,6 +7,12 @@ export interface User {
   email_verified_at?: string;
 }
 
+export interface Todo {
+  title: string;
+  created_at: string;
+  updated_at: string;
+}
+
 export type PageProps<
   T extends Record<string, unknown> = Record<string, unknown>
 > = T & {
