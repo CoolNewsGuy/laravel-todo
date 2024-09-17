16d1a489c853671ca6309a5de0c0f026a0e14884
idiot <joewilliamdd31@gmail.com>
2024-09-17 21:59:05 +0100

    Make 'color' column

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
+ *
  *
  * @property int $id
  * @property string $color
@@ -26,4 +26,6 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
   use HasFactory;
+
+  protected $fillable = ['color'];
 }
