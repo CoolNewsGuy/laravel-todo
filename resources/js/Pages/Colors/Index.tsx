6b8865130c6112c9589eebaed128265f82b09c2c
idiot <joewilliamdd31@gmail.com>
2024-09-17 23:15:06 +0100

    Add sample colors if user hasn't added a color yet

--- resources/js/Pages/Colors/Index.tsx
+++ resources/js/Pages/Colors/Index.tsx
@@ -1,19 +1,27 @@
 import ColorCard from "@/Components/ColorCard";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Color, PageProps } from "@/types";
+import { Head, router } from "@inertiajs/react";
+import { useEffect } from "react";
 
-export default function Index() {
-  const sampleColors = ["#ace", "#f2c", "#2ec", "#9c2", "#ff8", "#2ff"];
+export default function Index({
+  colors = [],
+  auth,
+}: PageProps<{ colors: Color[] }>) {
+  useEffect(() => {
+    if (!auth.user.has_added_color) {
+      router.post("/add-sample-colors");
+    }
+  }, []);
 
   return (
     <AuthenticatedLayout>
       <Head title="Colors" />
 
       <div className="grid grid-cols-1 sm:w-[90%] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-10 justify-items-stretch w-[85%] mx-auto">
-        {sampleColors.map((color) => (
-          <ColorCard key={color} color={color} />
+        {colors.map((color) => (
+          <ColorCard key={color.id} color={color.color} />
         ))}
-        <ColorCard color="#aceece" />
       </div>
     </AuthenticatedLayout>
   );
