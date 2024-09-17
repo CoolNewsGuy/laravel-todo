781f4c907cd1f80bb51b950cd129d271aa53ceb9
idiot <joewilliamdd31@gmail.com>
2024-09-18 00:37:35 +0100

    Create convertTo6DigitsHex util

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * @returns `null` if unable to convert
+ */
+export function convertTo6DigitsHex(str: string): string | null {
+  const digitsAfterHashtag = str.slice(1);
+
+  if (str.startsWith("#") && [3, 4].includes(digitsAfterHashtag.length)) {
+    let result = "#";
+
+    for (const digit of digitsAfterHashtag) {
+      result = `${result}${digit.repeat(2)}`;
+    }
+
+    return result;
+  }
+
+  return null;
+}
