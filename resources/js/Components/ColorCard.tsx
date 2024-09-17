244078102e34b973669c51251fe773577219e0bc
idiot <joewilliamdd31@gmail.com>
2024-09-17 16:37:38 +0100

    Let user copy color from the card

--- resources/js/Components/ColorCard.tsx
+++ resources/js/Components/ColorCard.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { Check, Copy } from "lucide-react";
+import { useState } from "react";
 
 interface ColorCardProps {
   color: string;
@@ -6,9 +9,44 @@ interface ColorCardProps {
 }
 
 export default function ColorCard({ color, className }: ColorCardProps) {
+  const [copied, setCopied] = useState(false);
+
+  async function copyColorToClipboard() {
+    await navigator.clipboard.writeText(color);
+
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 500);
+  }
+
   return (
-    <div className={cn("rounded-md overflow-hidden", className)}>
-      <div className="h-[12rem]" style={{ backgroundColor: color }}></div>
+    <div
+      className={cn(
+        "rounded-md overflow-hidden transition-transform hover:scale-105",
+        className
+      )}
+    >
+      <div className="h-[12rem] relative" style={{ backgroundColor: color }}>
+        <Button
+          className={cn(
+            "absolute right-4 top-4 size-[2.1rem] border-gray-400 border transition-colors",
+            copied && "bg-green-400 border-transparent hover:bg-green-400"
+          )}
+          size={"icon"}
+          onClick={copyColorToClipboard}
+          aria-label={"Copy color to clipboard"}
+          title={"Copy color"}
+        >
+          {copied ? (
+            <Check className="stroke-white" size={20} />
+          ) : (
+            <Copy size={17} />
+          )}
+        </Button>
+      </div>
+
       <div className="flex justify-center text-lg uppercase p-3 border dark:bg-gray-800 dark:border-gray-700 rounded-b-md">
         {color}
       </div>
