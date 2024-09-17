579728a97f4150d1021c91181b11b5b3d774a65c
idiot <joewilliamdd31@gmail.com>
2024-09-17 14:06:07 +0100

    Create a colors' grid with sample colors

--- resources/js/Pages/Colors/Index.tsx
+++ resources/js/Pages/Colors/Index.tsx
@@ -1,10 +1,20 @@
+import ColorCard from "@/Components/ColorCard";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
 export default function Index() {
+  const sampleColors = ["#ace", "#f2c", "#2ec", "#9c2", "#ff8", "#2ff"];
+
   return (
     <AuthenticatedLayout>
       <Head title="Colors" />
+
+      <div className="grid grid-cols-1 sm:w-[90%] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-10 justify-items-stretch w-[85%] mx-auto">
+        {sampleColors.map((color) => (
+          <ColorCard key={color} color={color} />
+        ))}
+        <ColorCard color="#aceece" />
+      </div>
     </AuthenticatedLayout>
   );
 }
