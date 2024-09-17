b19cfd57de3bbf818cdb816b418604b98fe86056
idiot <joewilliamdd31@gmail.com>
2024-09-17 20:45:18 +0100

    Add 'has_added_color' field to users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
       $table->string('email')->unique();
       $table->timestamp('email_verified_at')->nullable();
       $table->string('password');
+      $table->boolean('has_added_color')->default(false);
       $table->rememberToken();
       $table->timestamps();
     });
