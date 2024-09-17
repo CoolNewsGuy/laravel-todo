a3121cda829949e75ca5b7638ec8e39c02738422
idiot <joewilliamdd31@gmail.com>
2024-09-17 13:15:58 +0100

    Create Colors page

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Color;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ColorController extends Controller
 {
@@ -12,7 +13,7 @@ class ColorController extends Controller
    */
   public function index()
   {
-    //
+    return Inertia::render('Colors/Index');
   }
 
   /**
