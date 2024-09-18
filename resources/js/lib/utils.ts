2766ebde6e58ea86688c89c78744e9ef9a748442
idiot <joewilliamdd31@gmail.com>
2024-09-18 18:53:22 +0100

    Edit getTheFullHexColorForm util

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -6,19 +6,29 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
- * @returns `null` if unable to convert
+ * Returns the full form of a hex color (either with 6 digits or 8 if an alpha channel was provided).
+ *
+ * If `hexStr` is already in the full form, it returns it.
+ *
+ * Otherwise returns `null`, if `hexStr` contains invalid hex digits.
  */
 export function getTheFullHexColorForm(hexStr: string): string | null {
   const digitsAfterHashtag = hexStr.slice(1);
 
-  if (hexStr.startsWith("#") && [3, 4].includes(digitsAfterHashtag.length)) {
-    let result = "#";
+  if (hexStr.startsWith("#") && containsOnlyHexDigits(hexStr.slice(1))) {
+    if ([3, 4].includes(digitsAfterHashtag.length)) {
+      let result = "#";
 
-    for (const digit of digitsAfterHashtag) {
-      result = `${result}${digit.repeat(2)}`;
+      for (const digit of digitsAfterHashtag) {
+        result = `${result}${digit.repeat(2)}`;
+      }
+
+      return result;
     }
 
-    return result;
+    if ([6, 8].includes(digitsAfterHashtag.length)) {
+      return hexStr;
+    }
   }
 
   return null;
