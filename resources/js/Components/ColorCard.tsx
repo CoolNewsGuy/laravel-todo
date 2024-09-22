19bd4e65b6af0c905cd259ebd500c4510126de1e
idiot <joewilliamdd31@gmail.com>
2024-09-22 07:23:36 +0100

    Use TooltipWrapper in ColorCard

--- resources/js/Components/ColorCard.tsx
+++ resources/js/Components/ColorCard.tsx
@@ -5,6 +5,7 @@ import { Color } from "@/types";
 import { router } from "@inertiajs/react";
 import { Check, Copy, Heart, Trash2 } from "lucide-react";
 import { useMemo, useState } from "react";
+import TooltipWrapper from "./TooltipWrapper";
 
 interface ColorCardProps {
   color: Color;
@@ -95,73 +96,75 @@ export default function ColorCard({ color, className }: ColorCardProps) {
   }
 
   return (
-    <div
-      className={cn(
-        "cursor-pointer overflow-hidden rounded-md shadow-md transition-transform hover:scale-105",
-        className,
-      )}
-      title="Copy color"
-      onDoubleClick={addToFavorite}
-      onClick={copyColorToClipboard}
-    >
+    <TooltipWrapper content="Copy color">
       <div
-        className="relative h-[13rem]"
-        style={{ backgroundColor: color.color }}
+        className={cn(
+          "cursor-pointer overflow-hidden rounded-md shadow-md transition-transform hover:scale-105",
+          className,
+        )}
+        onDoubleClick={addToFavorite}
+        onClick={copyColorToClipboard}
       >
-        <Button
-          className={cn(
-            "absolute right-4 top-4 size-[2.1rem] border border-gray-400 bg-white transition-colors hover:bg-white/90",
-            copied && "border-transparent bg-green-400 hover:bg-green-400",
-          )}
-          size={"icon"}
-          onClick={copyColorToClipboard}
-          aria-label={"Copy color to clipboard"}
-          title={"Copy color"}
+        <div
+          className="relative h-[13rem]"
+          style={{ backgroundColor: color.color }}
         >
-          {copied ? (
-            <Check className="stroke-white" size={20} />
-          ) : (
-            <Copy className="stroke-black" size={17} />
-          )}
-        </Button>
-        <Button
-          className={cn(
-            "absolute right-4 top-14 size-[2.1rem] border border-gray-400 bg-white text-black transition-colors duration-75 hover:border-red-400 hover:bg-red-50 hover:text-red-400",
-            addedToFavorite && "border-transparent bg-red-200 text-red-500",
-          )}
-          size={"icon"}
-          aria-label={"Add to favorite"}
-          title={"Add to favorite"}
-          onClick={addToFavorite}
-        >
-          <Heart className={cn(addedToFavorite && "fill-red-500")} size={20} />
-        </Button>
-        <Button
-          className={cn(
-            "absolute right-4 top-24 size-[2.1rem] bg-red-600 text-white transition-colors hover:bg-red-700 active:bg-red-800",
-          )}
-          size={"icon"}
-          aria-label={"Remove color"}
-          title={"Remove color"}
-          onClick={removeColor}
-        >
-          <Trash2 size={20} />
-        </Button>
-
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Heart
-            size={100}
+          <Button
             className={cn(
-              "scale-0 fill-red-500 stroke-red-300 stroke-1 transition-transform",
-              showBigHeart && "scale-100",
+              "absolute right-4 top-4 size-[2.1rem] border border-gray-400 bg-white transition-colors hover:bg-white/90",
+              copied && "border-transparent bg-green-400 hover:bg-green-400",
             )}
-          />
+            size={"icon"}
+            onClick={copyColorToClipboard}
+            aria-label={"Copy color to clipboard"}
+            title={"Copy color"}
+          >
+            {copied ? (
+              <Check className="stroke-white" size={20} />
+            ) : (
+              <Copy className="stroke-black" size={17} />
+            )}
+          </Button>
+          <Button
+            className={cn(
+              "absolute right-4 top-14 size-[2.1rem] border border-gray-400 bg-white text-black transition-colors duration-75 hover:border-red-400 hover:bg-red-50 hover:text-red-400",
+              addedToFavorite && "border-transparent bg-red-200 text-red-500",
+            )}
+            size={"icon"}
+            aria-label={"Add to favorite"}
+            title={"Add to favorite"}
+            onClick={addToFavorite}
+          >
+            <Heart
+              className={cn(addedToFavorite && "fill-red-500")}
+              size={20}
+            />
+          </Button>
+          <Button
+            className={cn(
+              "absolute right-4 top-24 size-[2.1rem] bg-red-600 text-white transition-colors hover:bg-red-700 active:bg-red-800",
+            )}
+            size={"icon"}
+            aria-label={"Remove color"}
+            title={"Remove color"}
+            onClick={removeColor}
+          >
+            <Trash2 size={20} />
+          </Button>
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Heart
+              size={100}
+              className={cn(
+                "scale-0 fill-red-500 stroke-red-300 stroke-1 transition-transform",
+                showBigHeart && "scale-100",
+              )}
+            />
+          </div>
+        </div>
+        <div className="flex justify-center rounded-b-md border p-3 text-lg uppercase dark:border-gray-700 dark:bg-gray-800">
+          {colorNameUpperCased}
         </div>
       </div>
-
-      <div className="flex justify-center rounded-b-md border p-3 text-lg uppercase dark:border-gray-700 dark:bg-gray-800">
-        {colorNameUpperCased}
-      </div>
-    </div>
+    </TooltipWrapper>
   );
 }
