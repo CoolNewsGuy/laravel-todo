cc2303af120d4593303a89a2a3b645adf1319243
idiot <joewilliamdd31@gmail.com>
2024-09-23 08:29:48 +0100

    Create ColorPreviewBox

--- resources/js/Components/NewColorForm.tsx
+++ resources/js/Components/NewColorForm.tsx
@@ -3,7 +3,12 @@ import PrimaryButton from "./PrimaryButton";
 import TextInput from "./TextInput";
 import { useForm } from "@inertiajs/react";
 import InputError from "./InputError";
-import { cn, containsOnlyHexDigits, generateRandomHexColor } from "@/lib/utils";
+import {
+  cn,
+  containsOnlyHexDigits,
+  generateRandomHexColor,
+  getTheFullHexColorForm,
+} from "@/lib/utils";
 import { useEffect, useState } from "react";
 import { useNotification } from "@/hooks/use-notification";
 import TooltipWrapper from "./TooltipWrapper";
@@ -15,6 +20,8 @@ export default function NewColorForm() {
     green: "",
     blue: "",
   });
+  const color = `#${data.red}${data.green}${data.blue}` as const;
+  const invalidDigitError = !containsOnlyHexDigits(color.slice(1));
   const [lengthError, setLengthError] = useState(false);
   const notification = useNotification();
 
@@ -63,74 +70,89 @@ export default function NewColorForm() {
   }, [data]);
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className="flex items-end gap-2">
-        <TooltipWrapper content="Generate a random color">
-          <div className="grid size-[3.3rem] place-content-center rounded-full">
-            <SecondaryButton
-              type="button"
-              className="grid size-[2.8rem] place-content-center !rounded-full transition-transform hover:rotate-180"
-              aria-label="Generate random color"
-              onClick={() => {
-                const randomColor = generateRandomHexColor().slice(1);
-                setData({
-                  red: randomColor[0] + randomColor[1],
-                  green: randomColor[2] + randomColor[3],
-                  blue: randomColor[4] + randomColor[5],
-                });
-              }}
-            >
-              <Dices size={22} />
-            </SecondaryButton>
-          </div>
-        </TooltipWrapper>
+    <form onSubmit={onSubmit} className="w-[235px]">
+      <div className="flex flex-col gap-3">
+        <div className="flex items-end gap-2">
+          <TooltipWrapper content="Generate a random color">
+            <div className="grid size-[3.3rem] place-content-center rounded-full">
+              <SecondaryButton
+                type="button"
+                className="grid size-[2.8rem] place-content-center !rounded-full transition-transform hover:rotate-180"
+                aria-label="Generate random color"
+                onClick={() => {
+                  const randomColor = generateRandomHexColor().slice(1);
+                  setData({
+                    red: randomColor[0] + randomColor[1],
+                    green: randomColor[2] + randomColor[3],
+                    blue: randomColor[4] + randomColor[5],
+                  });
+                }}
+              >
+                <Dices size={22} />
+              </SecondaryButton>
+            </div>
+          </TooltipWrapper>
 
-        {(["red", "green", "blue"] as const).map((color) => (
-          <div className="flex flex-col items-center gap-2" key={color}>
-            <label htmlFor={color} className="text-lg text-muted-foreground">
-              {color[0].toUpperCase()}
-            </label>
-            <TextInput
-              id={color}
-              className="size-[3.3rem] p-3 text-center text-lg uppercase"
-              maxLength={2}
-              value={data[color]}
-              onChange={(e) => {
-                setData(color, e.target.value.toUpperCase());
-              }}
-              isFocused={!!errors[color]}
-            />
-          </div>
-        ))}
-
-        <div className="ml-2 flex flex-col items-center gap-2">
-          <label className="invisible text-lg">s</label>
-          <PrimaryButton
-            className="size-[3.3rem] dark:bg-indigo-800 dark:text-white dark:hover:!bg-indigo-900 dark:focus:!bg-indigo-900 dark:active:bg-indigo-900"
-            disabled={processing}
-          >
-            <Plus />
-          </PrimaryButton>
+          {(["red", "green", "blue"] as const).map((color) => (
+            <div className="flex flex-col items-center gap-2" key={color}>
+              <label htmlFor={color} className="text-lg text-muted-foreground">
+                {color[0].toUpperCase()}
+              </label>
+              <TextInput
+                id={color}
+                className="size-[3.3rem] p-3 text-center text-lg uppercase"
+                maxLength={2}
+                value={data[color]}
+                onChange={(e) => {
+                  setData(color, e.target.value.toUpperCase());
+                }}
+                isFocused={!!errors[color]}
+              />
+            </div>
+          ))}
         </div>
-      </div>
 
-      {!containsOnlyHexDigits(`${data.red}${data.green}${data.blue}`) && (
-        <InputError
-          message="Invalid hex digits."
-          className="mt-2 text-[0.9rem] font-bold"
-        />
-      )}
-
-      {lengthError && (
-        <InputError
-          message="All fields must be either of length 1 or 2."
+        <div
           className={cn(
-            "text-[0.9rem] font-bold",
-            containsOnlyHexDigits(`${data.red}${data.green}${data.blue}`) &&
-              "mt-2",
+            "space-y-1",
+            !invalidDigitError && !lengthError && "hidden",
           )}
-        />
-      )}
+        >
+          {invalidDigitError && (
+            <InputError message="Invalid hex digits." className="font-bold" />
+          )}
+
+          {lengthError && (
+            <InputError
+              message="All fields must be either of length 1 or 2."
+              className={cn("font-bold leading-4")}
+            />
+          )}
+        </div>
+
+        <ColorPreviewBox color={color} />
+
+        <PrimaryButton
+          className="h-[3.3rem] place-content-center !text-base capitalize tracking-wide dark:bg-indigo-800 dark:text-white dark:hover:!bg-indigo-900 dark:focus:!bg-indigo-900 dark:active:bg-indigo-900"
+          disabled={processing}
+        >
+          Add color
+        </PrimaryButton>
+      </div>
     </form>
+  );
+}
+
+function ColorPreviewBox({ color }: { color: string }) {
+  return (
+    <TooltipWrapper content="Color preview">
+      <div
+        className={cn(
+          "size-[235px] rounded-md border-2 border-indigo-800",
+          getTheFullHexColorForm(color) && "border-none",
+        )}
+        style={{ backgroundColor: color }}
+      ></div>
+    </TooltipWrapper>
   );
 }
