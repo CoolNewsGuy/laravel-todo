dbbdb337926a5a4b6b040eecdc93e5e63c3dbe4a
idiot <joewilliamdd31@gmail.com>
2024-09-24 08:11:45 +0100

    Add 'comments' relationships to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,4 +81,9 @@ class User extends Authenticatable
   {
     return $this->hasMany(Color::class);
   }
+
+  public function comments(): HasMany
+  {
+    return $this->hasMany(Comment::class);
+  }
 }
